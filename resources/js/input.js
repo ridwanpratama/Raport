f98309b2f71e5de9a9a998727526e29fc3bbb685
Erick Christian <erickchristian48@gmail.com>
2021-03-25 18:58:09 +0700

    task: moreMapel done

--- resources/js/input.js
+++ resources/js/input.js
@@ -6,8 +6,22 @@ const pengetahuanInputs = document.querySelectorAll(
 const keterampilanInputs = document.querySelectorAll(
   'input[data-tag="keterampilanInput"]'
 )
+const moreMapel = document.querySelector('#moreMapel')
+const lessMapel = document.querySelector('#lessMapel')
+const baseForm = document.querySelector('#baseForm')
+const formsParent = document.querySelector('#formsParent')
 
 nilaiSelect.addEventListener('change', ({ target: { value } }) => {
   keterampilanInputs.forEach(input => input.setAttribute('name', `${value}[]`))
   pengetahuanInputs.forEach(input => input.setAttribute('name', `${value}[]`))
+})
+
+moreMapel.addEventListener('click', () => {
+  const formClone = baseForm.cloneNode(true)
+  formsParent.appendChild(formClone)
+})
+
+lessMapel.addEventListener('click', () => {
+  if (formsParent.children.length > 2)
+    formsParent.removeChild(formsParent.lastElementChild)
 })
